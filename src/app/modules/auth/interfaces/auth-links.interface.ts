0ff80c882b77ee5c3b1b667ab178ec5d3570224d
scotechlab <sco.techlab@gmail.com>
2026-03-05 16:51:29 +0100

    change file name

--- src/app/modules/auth/interfaces/auth-links.interface.ts
+++ src/app/modules/auth/interfaces/auth-links.interface.ts
@@ -1,8 +1,8 @@
+export interface IAuthLinksComponent {
+  links?: IAuthLink[];
+}
+
 export interface IAuthLink {
   linkLabel?: string;
   linkUrl?: string;
-}
-
-export interface IAuthLinksComponent {
-  links?: IAuthLink[];
 }
--- src/app/modules/auth/interfaces/index.ts
+++ src/app/modules/auth/interfaces/index.ts
@@ -2,4 +2,4 @@ export * from './auth-card.interface';
 export * from './auth-event.interface';
 export * from './auth-header.interface';
 export * from './auth-input.interface';
-export * from './auth-link.interface';
+export * from './auth-links.interface';
