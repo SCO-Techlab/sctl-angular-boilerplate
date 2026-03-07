cb604f53cf62d087f78c8a55d9d7de40f5d8db7d
scotechlab <sco.techlab@gmail.com>
2026-03-07 02:20:06 +0100

    add update user info http

--- src/app/modules/profile/components/profile-personal-information/profile-personal-information.component.ts
+++ src/app/modules/profile/components/profile-personal-information/profile-personal-information.component.ts
@@ -2,11 +2,13 @@ import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileService } from '@modules/profile/services';
+import { Store } from '@ngxs/store';
+import { SetAccessToken } from '@session-storage';
 import { InputErrorComponent } from '@shared/components';
 import { DATES, MAGIC_NUMBERS } from '@shared/constants';
-import { ITranslateLiterals, IUser } from '@shared/interfaces';
+import { IJwtToken, ITranslateLiterals, IUser } from '@shared/interfaces';
 import { TranslateModule } from '@shared/modules';
-import { DatesService, SpinnerService, ToastService, TranslateService } from '@shared/services';
+import { DatesService, JwtTokenService, SpinnerService, ToastService, TranslateService } from '@shared/services';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { finalize } from 'rxjs';
@@ -33,11 +35,13 @@ export class ProfilePersonalInformationComponent implements OnInit {
   private literals: ITranslateLiterals;
 
   private destroyRef$ = inject(DestroyRef);
+  private store = inject(Store);
   private translateService = inject(TranslateService);
   private profileService = inject(ProfileService);
   private datesService = inject(DatesService);
   private spinnerService = inject(SpinnerService);
   private toastService = inject(ToastService);
+  private tokenService = inject(JwtTokenService);
 
   ngOnInit(): void {
     this.initForm();
@@ -66,12 +70,16 @@ export class ProfilePersonalInformationComponent implements OnInit {
         finalize(() => this.spinnerService.hide())
       )
       .subscribe({
-        next: (updatedUser: IUser) => {
-          if (!updatedUser) {
+        next: (token: IJwtToken) => {
+          if (!token?.accessToken) {
             this.toastService.error({ summary: this.translateService.instant('TOAST.ERROR'), detail: this.literals['REQUEST_KO'] });
             return;
           }
 
+          this.store.dispatch(new SetAccessToken({ accessToken: token.accessToken }));
+          this.fillForm(this.tokenService.decodeToken(token.accessToken)?.user);
+          this.lockForm = true;
+          this.disableOrEnableForm(this.lockForm);
           this.toastService.success({ summary: this.translateService.instant('TOAST.SUCCESS'), detail: this.literals['REQUEST_OK'] });
         },
         error: () => {
@@ -90,6 +98,16 @@ export class ProfilePersonalInformationComponent implements OnInit {
     });
 
     this.disableOrEnableForm(this.lockForm);
+  }
+
+  private fillForm(user: IUser): void {
+    this.personalInformationForm.setValue({
+      personalName: user.personalName ?? '',
+      userName: user.userName ?? '',
+      email: user.email ?? '',
+      role: user.role.name ?? '',
+      createdAt: this.datesService.formatDate(DATES.ISO_DATE, user.createdAt) ?? ''
+    });
   }
 
   private formatRoleName(): string {
--- src/app/modules/profile/services/profile.service.ts
+++ src/app/modules/profile/services/profile.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@environment';
-import { IUser } from '@shared/interfaces';
+import { IJwtToken, IUser } from '@shared/interfaces';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,8 +11,8 @@ export class ProfileService {
 
   private http = inject(HttpClient);
 
-  updateUserInfo(_id: string, user: Partial<IUser>): Observable<IUser> {
+  updateUserInfo(_id: string, user: Partial<IUser>): Observable<IJwtToken> {
     const body = { ...user };
-    return this.http.put<IUser>(`${environment.apiUrl}/profile/update/user/info/${_id}`, body);
+    return this.http.put<IJwtToken>(`${environment.apiUrl}/profile/update/user/info/${_id}`, body);
   }
 }
