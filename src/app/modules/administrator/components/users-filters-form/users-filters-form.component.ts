289aa0397ec8c8fa68652635504eed4341b08bba
scotechlab <sco.techlab@gmail.com>
2026-05-15 03:21:34 +0200

    add roles selector

--- src/app/modules/administrator/components/users-filters-form/users-filters-form.component.ts
+++ src/app/modules/administrator/components/users-filters-form/users-filters-form.component.ts
@@ -1,19 +1,23 @@
-import { Component, DestroyRef, inject, OnInit, output } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, output, ViewEncapsulation } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IUser } from '@shared/interfaces';
+import { RolesService } from '@modules/administrator/services';
+import { IRole, IUser } from '@shared/interfaces';
 import { TranslateModule } from '@shared/modules';
 import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'sctl-users-filters-form',
   standalone: true,
   templateUrl: './users-filters-form.component.html',
+  encapsulation: ViewEncapsulation.None,
   imports: [
     FormsModule,
     ReactiveFormsModule,
     TranslateModule,
     InputTextModule,
+    SelectModule
   ]
 })
 export class UsersFiltersFormComponent implements OnInit {
@@ -21,20 +25,34 @@ export class UsersFiltersFormComponent implements OnInit {
   public valueChange = output<Partial<IUser>>();
 
   public form: FormGroup;
+  public rolesOptions: { name: string; value: string }[] = [];
 
-  private destroyRefs$ = inject(DestroyRef);
+  private destroyRef$ = inject(DestroyRef);
+  private rolesService = inject(RolesService);
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl(null),
-    });
-
-    this.form.valueChanges
-      .pipe(takeUntilDestroyed(this.destroyRefs$))
-      .subscribe(value => this.valueChange.emit(value));
+    this.initForm();
+    this.getRoles();
   }
 
   public clearForm(): void {
     this.form.reset();
+  }
+
+  private initForm(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null),
+      role: new FormControl(null)
+    });
+
+    this.form.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef$))
+      .subscribe(value => this.valueChange.emit(value));
+  }
+
+  private getRoles(): void {
+    this.rolesService.find(null)
+      .pipe(takeUntilDestroyed(this.destroyRef$))
+      .subscribe((res: IRole[]) => this.rolesOptions = res?.map(role => ({ name: role.name, value: role._id })) ?? []);
   }
 }
